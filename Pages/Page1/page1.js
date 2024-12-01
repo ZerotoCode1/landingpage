07e965910ad351100e99ab9c75447908098208c0
Quang <quang11102002f@gmail.com>
2024-12-01 13:47:29 +0700

    pag2

--- Pages/Page1/page1.js
+++ Pages/Page1/page1.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error loading header:', error));
 });
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('../../components/Header/header.html') // Đường dẫn đến file header
+        .then(response => response.text()) // Đọc nội dung file
+        .then(data => {
+            document.getElementById('header').innerHTML = data; // Chèn vào div#header
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('../../components/FeedBack/FeedBack.html') // Đường dẫn đến file header
+        .then(response => response.text()) // Đọc nội dung file
+        .then(data => {
+            document.getElementById('feedback-link').innerHTML = data; // Chèn vào div#header
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
--- homescript.js
+++ homescript.js
@@ -22,4 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error loading header:', error));
 });
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('./components/Header/header.html') // Đường dẫn đến file header
+        .then(response => response.text()) // Đọc nội dung file
+        .then(data => {
+            document.getElementById('header').innerHTML = data; // Chèn vào div#header
+        })
+        .catch(error => console.error('Error loading header:', error));
+});
 
